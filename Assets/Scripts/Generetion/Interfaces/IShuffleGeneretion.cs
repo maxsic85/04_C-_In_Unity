2981f05fa79a69f63a02011104e912a315516a36
Maksim.S <maxsic85@mail.ru>
2021-08-10 17:58:39 +0500

    homework02v2

---
+++ Assets/Scripts/Generetion/Interfaces/IShuffleGeneretion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Max.Generetion
+{
+    public interface IShuffleGeneretion : IMapGeneretion
+    {
+      public   void Shuffle();
+    }
+}
