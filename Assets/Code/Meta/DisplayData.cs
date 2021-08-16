954aa0c54adf344c4f275f5281d399dec4e901c9
Maksim.S <maxsic85@mail.ru>
2021-08-16 10:59:01 +0500

    fuckihn git wq :wq
    wq
    :wq
    i:qw
    wq
    iwq

---
+++ Assets/Code/Meta/DisplayData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Max.Meta
+{
+    public static class DisplayData
+    {
+        internal static float _durationShake = 1f;
+        internal static float _magnitudeShake = 0.4f;
+        internal static float _noiseShake = 100f;
+
+        internal static float _changeColorTime = 1f;
+
+        internal static Color _cameraBackColor = Color.black;
+        internal static Color _cameraBackChangeColor = Color.red;
+    }
+}
